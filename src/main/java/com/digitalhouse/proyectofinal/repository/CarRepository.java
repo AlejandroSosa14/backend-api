0f949733e6460a6d7be8129897dfc259e0b4d2d4
Jesus Olvera <jcolvera_s@outlook.com>
2025-02-14 17:06:38 -0600

    Add repositorio Car

--- src/main/java/com/digitalhouse/proyectofinal/repository/CarRepository.java
+++ src/main/java/com/digitalhouse/proyectofinal/repository/CarRepository.java
@@ -1,4 +1,13 @@
 package com.digitalhouse.proyectofinal.repository;
 
-public interface CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.digitalhouse.proyectofinal.entity.CarEntity;
+import java.util.List;
+
+
+public interface CarRepository extends JpaRepository<CarEntity,Long>{
+
+    public List<CarEntity> findByTransmissionType(String transmissionType);
+
 }
