83b51330aa95c869a1d5d6aacf6faebfc2229025
jcos-dev <jcolvera_s@outlook.com>
2025-03-03 19:29:37 -0600

    Add validar longitud de contrasenia y formato de email

--- src/main/java/com/digitalhouse/proyectofinal/entity/UserEntity.java
+++ src/main/java/com/digitalhouse/proyectofinal/entity/UserEntity.java
@@ -1,12 +1,12 @@
 package com.digitalhouse.proyectofinal.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @NoArgsConstructor
@@ -24,11 +24,13 @@ public class UserEntity {
 
     @Setter
     @NotBlank
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", flags = Pattern.Flag.CASE_INSENSITIVE)
     @Column(unique = true)
     private String email;
 
     @Setter
     @NotBlank
+    @Length(min = 8)
     private String password;
 
     @Setter
