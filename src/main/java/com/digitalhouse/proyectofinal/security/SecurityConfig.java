f0a99035c10df35d0239a683e34c8d1b3dc65e02
jcos-dev <jcolvera_s@outlook.com>
2025-03-27 18:42:07 -0600

    Fix crear usuario proporcionando los datos de registro

--- src/main/java/com/digitalhouse/proyectofinal/security/SecurityConfig.java
+++ src/main/java/com/digitalhouse/proyectofinal/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/users/favorites").hasAnyRole("admin","customer")
                         .requestMatchers(HttpMethod.GET, "/api/users/favorites/*").hasAnyRole("admin","customer")
                         .requestMatchers(HttpMethod.PUT, "/api/users/favorites/**").hasAnyRole("admin","customer")
-                        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("admin")
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("admin")
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("admin")
                         .requestMatchers(HttpMethod.GET, "/api/categories/**").hasRole("admin")
