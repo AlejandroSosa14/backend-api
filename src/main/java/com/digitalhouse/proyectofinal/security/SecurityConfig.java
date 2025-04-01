46a11499be3bf1484a12ef85808a512d234d61ba
jcos-dev <jcolvera_s@outlook.com>
2025-03-31 21:50:48 -0600

    Update abrir consulta datos de cliente

--- src/main/java/com/digitalhouse/proyectofinal/security/SecurityConfig.java
+++ src/main/java/com/digitalhouse/proyectofinal/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/cars/**").hasRole("admin")
                         .requestMatchers(HttpMethod.POST, "/api/cars").hasRole("admin")
                         .requestMatchers(HttpMethod.PUT, "/api/cars/**").hasRole("admin")
-                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("admin")
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("admin","customer")
                         .requestMatchers(HttpMethod.POST, "/api/users/favorites").hasAnyRole("admin","customer")
                         .requestMatchers(HttpMethod.GET, "/api/users/favorites/*").hasAnyRole("admin","customer")
                         .requestMatchers(HttpMethod.PUT, "/api/users/favorites/**").hasAnyRole("admin","customer")
