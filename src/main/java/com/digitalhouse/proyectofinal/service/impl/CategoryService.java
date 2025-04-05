0b4b6ccfc371209817725153b8380c767069b30a
AlejandroSosa14 <alej4ndro1499@gmail.com>
2025-04-05 16:00:49 -0600

    Arregla muestra de la imagen de categoria despues de crearla en el front

--- src/main/java/com/digitalhouse/proyectofinal/service/impl/CategoryService.java
+++ src/main/java/com/digitalhouse/proyectofinal/service/impl/CategoryService.java
@@ -6,6 +6,7 @@ import com.digitalhouse.proyectofinal.exception.ConflictException;
 import com.digitalhouse.proyectofinal.exception.ResourceNotFoundException;
 import com.digitalhouse.proyectofinal.repository.CategoryRepository;
 import com.digitalhouse.proyectofinal.service.ICategoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jdk.jfr.Category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -39,12 +40,13 @@ public class CategoryService implements ICategoryService {
     }
 
     public CategoryEntity create(CategoryEntity categoryEntity, String dir, MultipartFile file) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
         if (file != null && !file.isEmpty()) { // Asegurar que el archivo no está vacío
             String uploadedFileUrl = fileUploadService.uploadFiles(dir, List.of(file));
             System.out.println("URL de la imagen subida: " + uploadedFileUrl); // 🔍 Verificar URL en logs
 
             if (uploadedFileUrl != null && !uploadedFileUrl.isEmpty()) {
-                categoryEntity.setImage(uploadedFileUrl);
+                categoryEntity.setImage(objectMapper.writeValueAsString(uploadedFileUrl));
             }
         }
         CategoryEntity savedCategory = categoryRepository.save(categoryEntity);
